ec61dea6add9ef7bfb5c107ee455d2760f6ebf61
John Bunting <codingjester@gmail.com>
2011-10-24 17:03:45 -0400

    Adding redis to the backend.

--- src/main/scala/storage.scala
+++ src/main/scala/storage.scala
@@ -1,5 +1,10 @@
 package bitshow
 
+import org.sedis._
+import redis.clients.jedis._
+import org.apache.commons.codec.digest.DigestUtils._
+import org.apache.commons.codec.binary.Base64._
+
 case class Item(contentType: String, bytes: Array[Byte])
 
 trait Storage {
@@ -20,4 +25,29 @@ trait VectorStore extends Storage { self =>
       vector = vector :+ bytes
       (vector.length - 1).toString
     }
-}  
+} 
+
+class RedisStorage extends Storage {
+  
+  val pool = new Pool(new JedisPool(new JedisPoolConfig(), "localhost", 6379, 2000));
+  val baser = new Base64()
+
+  override def get(id: String): Option[Item] = {
+    val res = pool.withClient { client =>
+       client.get(id) match {
+          s: String => Some(Item(hash, baser.decodeBase64String(filebytes)))
+          _ => None
+       }
+    }
+    return res
+  }
+  override def put(item: Item): String = {
+    val hash = pool.withClient { client =>
+        val hash = DigestUtils.sha256Hex(item.bytes)
+        val filebytes = baser.encodeBase64String(item.bytes)
+        client.set(hash, filebytes)
+        hash
+    }
+    hash
+  }
+}   
